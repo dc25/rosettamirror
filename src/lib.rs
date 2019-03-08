104c21b897d3f9ea89acb6e6a3e23c74989aa818
Dave Compton <davecompton7@gmail.com>
2019-03-08 11:52:04 -0800

    RosettaError changed to Error

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use std::fs::*;
 use std::io::prelude::*;
 
 use std::io::Read;
-use self::url::{Url};
+use self::url::Url;
 
 use rustc_serialize::json::{self, Json};
 
@@ -15,7 +15,7 @@ struct Task {
     pub title: String,
 }
 
-pub enum RosettaError {
+pub enum Error {
     /// Something went wrong with the HTTP request to the API.
     Http(reqwest::Error),
  
@@ -31,31 +31,31 @@ pub enum RosettaError {
     /// Unexpected JSON format from response
     UnexpectedFormat,
 }
-impl From<json::ParserError> for RosettaError {
+impl From<json::ParserError> for Error {
     fn from(error: json::ParserError) -> Self {
-        RosettaError::Json(error)
+        Error::Json(error)
     }
 }
  
-impl From<reqwest::Error> for RosettaError {
+impl From<reqwest::Error> for Error {
     fn from(error: reqwest::Error) -> Self {
-        RosettaError::Http(error)
+        Error::Http(error)
     }
 }
  
-impl From<std::io::Error> for RosettaError {
+impl From<std::io::Error> for Error {
     fn from(error: std::io::Error) -> Self {
-        RosettaError::Io(error)
+        Error::Io(error)
     }
 }
  
-impl From<self::url::ParseError> for RosettaError {
+impl From<self::url::ParseError> for Error {
     fn from(error: self::url::ParseError) -> Self {
-        RosettaError::ParseUrl(error)
+        Error::ParseUrl(error)
     }
 }
  
-fn construct_query_category(category: &str) -> Result<Url, RosettaError> {
+fn construct_query_category(category: &str) -> Result<Url, Error> {
     let mut base_url = Url::parse("http://rosettacode.org/mw/api.php")?;
     let cat = format!("Category:{}", category);
     let query_pairs = vec![("action", "query"),
@@ -68,7 +68,7 @@ fn construct_query_category(category: &str) -> Result<Url, RosettaError> {
     Ok(base_url)
 }
  
-fn construct_query_task_content(task_id: &str) -> Result<Url, RosettaError> {
+fn construct_query_task_content(task_id: &str) -> Result<Url, Error> {
     let mut base_url = Url::parse("http://rosettacode.org/mw/api.php")?;
     let mut query_pairs =
         vec![("action", "query"), ("format", "json"), ("prop", "revisions"), ("rvprop", "content")];
@@ -77,7 +77,7 @@ fn construct_query_task_content(task_id: &str) -> Result<Url, RosettaError> {
     Ok(base_url)
 }
  
-fn query_api(url: Url) -> Result<Json, RosettaError> {
+fn query_api(url: Url) -> Result<Json, Error> {
     let mut response = (reqwest::get(url.as_str()))?;
     // Build JSON
     let mut body = String::new();
@@ -86,14 +86,14 @@ fn query_api(url: Url) -> Result<Json, RosettaError> {
     Ok((Json::from_str(&body))?)
 }
  
-fn parse_all_tasks(reply: &Json) -> Result<Vec<Task>, RosettaError> {
-    let json_to_task = |json: &Json| -> Result<Task, RosettaError> {
+fn parse_all_tasks(reply: &Json) -> Result<Vec<Task>, Error> {
+    let json_to_task = |json: &Json| -> Result<Task, Error> {
         let page_id: u64 = (json.find("pageid")
             .and_then(|id| id.as_u64())
-            .ok_or(RosettaError::UnexpectedFormat))?;
+            .ok_or(Error::UnexpectedFormat))?;
         let title: &str = (json.find("title")
             .and_then(|title| title.as_string())
-            .ok_or(RosettaError::UnexpectedFormat))?;
+            .ok_or(Error::UnexpectedFormat))?;
  
         Ok(Task {
             page_id: page_id,
@@ -102,37 +102,37 @@ fn parse_all_tasks(reply: &Json) -> Result<Vec<Task>, RosettaError> {
     };
     let tasks_json = (reply.find_path(&["query", "categorymembers"])
         .and_then(|tasks| tasks.as_array())
-        .ok_or(RosettaError::UnexpectedFormat))?;
+        .ok_or(Error::UnexpectedFormat))?;
  
     // Convert into own type
     tasks_json.iter().map(json_to_task).collect()
 }
-fn get_task(task: &Json, task_id: u64) -> Result<String, RosettaError> {
+fn get_task(task: &Json, task_id: u64) -> Result<String, Error> {
     let revisions =
         (task.find_path(&["query", "pages", task_id.to_string().as_str(), "revisions"])
             .and_then(|content| content.as_array())
-            .ok_or(RosettaError::UnexpectedFormat))?;
+            .ok_or(Error::UnexpectedFormat))?;
     let content = (revisions[0]
         .find("*")
         .and_then(|content| content.as_string())
-        .ok_or(RosettaError::UnexpectedFormat))?;
+        .ok_or(Error::UnexpectedFormat))?;
     Ok(String::from(content))
 }
  
-fn query_all_tasks() -> Result<Vec<Task>, RosettaError> {
+fn query_all_tasks() -> Result<Vec<Task>, Error> {
     let query = construct_query_category("Programming_Tasks")?;
     let json = query_api(query)?;
     parse_all_tasks(&json)
 }
  
-fn query_a_task(task: &Task) -> Result<String, RosettaError> {
+fn query_a_task(task: &Task) -> Result<String, Error> {
     let query = construct_query_task_content(&task.page_id.to_string())?;
     let json = query_api(query)?;
     get_task(&json, task.page_id)
 }
 
 
-pub fn run(dir: &str) -> Result<(), RosettaError> {
+pub fn run(dir: &str) -> Result<(), Error> {
     let all_tasks = query_all_tasks()?;
     for task in &all_tasks {
         let content = query_a_task(task)?;

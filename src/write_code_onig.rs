739fe2a3ba0e847b12fd075c7b2c1e98c4ffafc3
Dave Compton <davecompton7@gmail.com>
2019-03-17 11:34:38 -0700

    removing nowiki blocks before doing more text processing

--- src/write_code_onig.rs
+++ src/write_code_onig.rs
@@ -10,10 +10,13 @@ pub fn write_code<T: Write>(log: &mut BufWriter<T>, dir: &String, code: &str) ->
 
     fs::DirBuilder::new().recursive(true).create(&dir)?;
 
+    let re = Regex::new(r"(?m)\<nowiki\>.*?\</nowiki\>")?;
+    let code = re.replace_all(code, "REMOVEDNOWIKI");
+
     let header_re = Regex::new(r"(?m)^===*\{\{[Hh]eader\|(.*?)\}\}(.*?)(?:\z|(?=^===*\{\{[Hh]eader))")?;
     let program_re = Regex::new(r"(?mi)<lang *(?: [^>]+)?>(.*?)<\/lang *>")?;
 
-    for header_match in header_re.captures_iter(code) {
+    for header_match in header_re.captures_iter(&code) {
         let lang = header_match.at(1).ok_or(RosettaError::UnexpectedFormat)?;
         writeln!(log, "{}", lang)?;
         let program_code = header_match.at(2).ok_or(RosettaError::UnexpectedFormat)?;

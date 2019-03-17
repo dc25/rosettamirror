c112e6bb803779efe16e035acb8a2215512ebfbd
Dave Compton <davecompton7@gmail.com>
2019-03-17 11:28:05 -0700

    slightly improved error handling - using if let instead of match

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,8 +4,7 @@ use rosettamirror;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let dir = &args[1];
-    match rosettamirror::run(dir) {
-        Err(s) => println! ("{:?}", s),
-        Ok(_) => (),
+    if let Err(s) = rosettamirror::run(dir) {
+        println! ("{:?}", s);
     }
 }

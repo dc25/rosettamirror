d3d8aad8bc9596ab94a08831ee0ca5a60698c01c
Dave Compton <davecompton7@gmail.com>
2019-03-18 11:27:44 -0700

    using map now to get through regular expression matching.  Lots of intermediate variables currently.

--- src/write_code_onig.rs
+++ src/write_code_onig.rs
@@ -54,8 +54,18 @@ pub fn write_code(dir: &str, task_name: &str, code: &str) -> Result<(), Box<dyn 
 
 
         let program_code = header_match.at(2).ok_or(RosettaError::UnexpectedFormat)?;
-        for program_match in program_re.captures_iter(program_code) {
-            let program  = program_match.at(1).ok_or(RosettaError::UnexpectedFormat)?;
+
+        let programs_it : onig::FindCaptures = program_re.captures_iter(program_code) ;
+        // let programs_it2 : std::iter::Map<onig::FindCaptures, Option<&str>> = programs_it
+        //                                           .map(|pm:std::ops::FnMut<(onig::find::Captures<'_>,)>| pm.at(1));
+        let programs_it2:Option<Vec<_>> = programs_it
+                                                  .map(|pm| pm.at(1)).collect();
+                                                  
+
+        let programs_it3: Result<Vec<_>,_> = programs_it2.ok_or(RosettaError::UnexpectedFormat);
+        let programs_it4 = programs_it3?;
+        // let programs:Result<Vec<&str>, Box<dyn Error>> = programs_it.collect()?;
+        for program in programs_it4.iter() {
             let program_name =   program_dir.clone()
                                + "/" 
                                + &task_file_name 
@@ -64,7 +74,7 @@ pub fn write_code(dir: &str, task_name: &str, code: &str) -> Result<(), Box<dyn 
 
 			let f = File::create(&program_name)?;
 			let mut f = BufWriter::new(f);
-			f.write_all(&program.as_bytes())?;
+			f.write_all(program.as_bytes())?;
         }
     }
     Ok(())

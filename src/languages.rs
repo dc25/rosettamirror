6514e5403f1667027f2ef242228487ac404401a9
Dave Compton <davecompton7@gmail.com>
2019-03-22 05:04:55 -0700

    first pass (untested) at creating map for langage names

--- src/languages.rs
+++ src/languages.rs
@@ -1,7 +1,8 @@
-use crate::error::RosettaError;
 use std::error::*;
 use std::collections::*;
+use onig::Regex;
 use crate::LanguageQuery;
+
 
 pub struct Languages {
     names: HashMap<String, String>  // map from lowercase name to name of choice
@@ -9,6 +10,20 @@ pub struct Languages {
 
 impl Languages {
     pub fn new(langs: &LanguageQuery) -> Result<Languages, Box<dyn Error>> {
-        Ok(Languages{names: std::collections::HashMap::new()})
+        let trim_cat_re = Regex::new(r"Category:")?;
+
+        let names: Vec<String> 
+            = langs.categorymembers.iter()
+                                   .map(|n| trim_cat_re.replace(&n.title,""))
+                                   .collect();
+
+        let name_pairs: Vec<(String,String)> 
+            = names.into_iter()
+                   .map(|n| (n.to_lowercase(), n.clone()))
+                   .collect();
+
+        let name_map = name_pairs.into_iter().collect();
+
+        Ok(Languages{names: name_map})
     }
 }

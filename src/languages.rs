ead2fbcacccb218371498b71975e07440b5463e8
Dave Compton <davecompton7@gmail.com>
2019-03-22 05:21:49 -0700

    cleaned up language map creation.

--- src/languages.rs
+++ src/languages.rs
@@ -4,6 +4,7 @@ use onig::Regex;
 use crate::LanguageQuery;
 
 
+#[derive(Debug)]
 pub struct Languages {
     names: HashMap<String, String>  // map from lowercase name to name of choice
 }
@@ -12,17 +13,12 @@ impl Languages {
     pub fn new(langs: &LanguageQuery) -> Result<Languages, Box<dyn Error>> {
         let trim_cat_re = Regex::new(r"Category:")?;
 
-        let names: Vec<String> 
-            = langs.categorymembers.iter()
-                                   .map(|n| trim_cat_re.replace(&n.title,""))
-                                   .collect();
-
-        let name_pairs: Vec<(String,String)> 
-            = names.into_iter()
-                   .map(|n| (n.to_lowercase(), n.clone()))
-                   .collect();
-
-        let name_map = name_pairs.into_iter().collect();
+        let name_map
+            = langs.categorymembers
+                       .iter()
+                       .map(|n| trim_cat_re.replace(&n.title,""))
+                       .map(|n| (n.to_lowercase(), n))
+                       .collect();
 
         Ok(Languages{names: name_map})
     }

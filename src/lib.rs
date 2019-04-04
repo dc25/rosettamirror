d3ed5c685937c443861d11b4060eadf71bb9dfa0
Dave Compton <davecompton7@gmail.com>
2019-04-04 00:49:18 +0000

    hand merged misc changes from branch getrevisions.

--- src/lib.rs
+++ src/lib.rs
@@ -17,33 +17,25 @@ mod write_code_onig;
 mod error;
 mod languages;
 
+// This post was helpful for getting impl Iterator argument working.
+// https://stackoverflow.com/a/34745885/509928
+
 pub trait CategoryQuery {
     fn new() -> Self;
     fn extend(self: &mut Self, other: Self);
-    fn query(cont_args: &[(String, String)]) -> Result<String, Box<dyn Error>>;
+    fn query(cont_args: impl Iterator<Item = (String,String)>) -> Result<String, Box<dyn Error>>;
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct TaskData {
     pageid: u64,
     title: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct TaskQuery {
     categorymembers: Vec<TaskData>
 }
-
-#[derive(Deserialize, Debug)]
-pub struct LanguageData {
-    title: String
-}
-
-#[derive(Deserialize, Debug)]
-pub struct LanguageQuery {
-    categorymembers: Vec<LanguageData>
-}
-
 
 impl CategoryQuery for TaskQuery {
 
@@ -56,10 +48,20 @@ impl CategoryQuery for TaskQuery {
             self.categorymembers.extend(other.categorymembers)
         }
 
-        fn query(cont_args: &[(String, String)]) -> Result<String, Box<dyn Error>> {
+        fn query(cont_args: impl Iterator<Item = (String,String)>) -> Result<String, Box<dyn Error>> {
           query_category(&"Programming_Tasks", cont_args)
         }
 
+}
+
+#[derive(Deserialize, Debug)]
+pub struct LanguageData {
+    title: String
+}
+
+#[derive(Deserialize, Debug)]
+pub struct LanguageQuery {
+    categorymembers: Vec<LanguageData>
 }
 
 impl CategoryQuery for LanguageQuery {
@@ -73,7 +75,7 @@ impl CategoryQuery for LanguageQuery {
             self.categorymembers.extend(other.categorymembers)
         }
 
-        fn query(cont_args: &[(String, String)]) -> Result<String, Box<dyn Error>> {
+        fn query(cont_args: impl Iterator<Item = (String,String)>) -> Result<String, Box<dyn Error>> {
           query_category(&"Programming_Languages", cont_args)
         }
 }
@@ -85,22 +87,31 @@ fn query_api(url: url::Url) -> Result<String, Box<dyn Error>> {
  
     Ok(body)
 }
- 
-fn query_category(cname: &str, cont_args: &[(String, String)]) -> Result<String, Box<dyn Error>> {
+
+/*
+http  rosettacode.org/mw/api.php               \
+        action==query                          \
+        format==json                           \
+        list==recentchanges                    \
+        'rcprop==title|ids'                    \
+        rclimit==450                           \
+        continue==
+*/
+
+fn query_category(cname: &str, cont_args: impl Iterator<Item = (String,String)>) -> Result<String, Box<dyn Error>> {
     let mut query = url::Url::parse("http://rosettacode.org/mw/api.php")?;
 
-    let cspec = "Category:".to_owned() + cname;
     let query_pairs 
-        = vec![ ("action", "query")
+        =     [ ("action", "query")
               , ("format", "json")
               , ("formatversion", "2")
               , ("list", "categorymembers")
               , ("cmlimit", "200")
-              , ("cmtitle", &cspec)
+              , ("cmtitle", &("Category:".to_owned() + cname))
               ];
 
     query.query_pairs_mut().extend_pairs(query_pairs.into_iter());
-    query.query_pairs_mut().extend_pairs(cont_args.iter());
+    query.query_pairs_mut().extend_pairs(cont_args);
     let json = query_api(query)?;
     Ok(json)
 }
@@ -108,15 +119,13 @@ fn query_category(cname: &str, cont_args: &[(String, String)]) -> Result<String,
 fn query_a_task(task: &TaskData) -> Result<String, Box<dyn Error>> {
     let mut query = url::Url::parse("http://rosettacode.org/mw/api.php")?;
 
-    let tp = &task.pageid.to_string();
-
     let query_pairs 
-        = vec![ ("action", "query")
+        =     [ ("action", "query")
               , ("format", "json")
               , ("formatversion", "2")
               , ("prop", "revisions")
               , ("rvprop", "content")
-              , ("pageids", tp)
+              , ("pageids", &task.pageid.to_string())
               ];
 
     query.query_pairs_mut().extend_pairs(query_pairs.into_iter());
@@ -128,11 +137,11 @@ fn query_all<'a, T: Deserialize<'a> + CategoryQuery>() -> Result<T, Box<dyn Erro
 
     let mut all_tasks = T::new();
 
-    let mut cont_args : Vec<(String, String)> 
-                = vec![("continue".to_owned(), "".to_owned())];
+    let mut cont_args = 
+                vec![("continue".to_owned(), "".to_owned())];
 
     loop {
-        let tasks_string = T::query(&cont_args)?;
+        let tasks_string = T::query(cont_args.into_iter())?;
         let tasks_value: Value = serde_json::from_str(&tasks_string)?;
         let query_value = tasks_value["query"].clone(); // why is this clone() necessary ?
         let query:T = T::deserialize(query_value)?;
@@ -141,20 +150,18 @@ fn query_all<'a, T: Deserialize<'a> + CategoryQuery>() -> Result<T, Box<dyn Erro
         let cont_value = &tasks_value["continue"];
         if cont_value.is_object() {
 
-            let cont_object = cont_value
-                                  .as_object()
-                                  .ok_or(RosettaError::UnexpectedFormat)?;
-
-            let to_cont_pair = |ca: (&String, &Value)| { 
+            let to_cont_pair = |ca: (&String, &Value)| -> Result<_, Box<dyn Error>> { 
                 let cp1 = ca.1.as_str().ok_or(RosettaError::UnexpectedFormat)?;
                 Ok((ca.0.clone(), cp1.to_owned()))
             };
 
-            let cont_args_result : Result<Vec<(String,String)>, Box<dyn Error>>
-                    = cont_object 
-                        .iter().map(to_cont_pair).collect();
+            cont_args = cont_value 
+                            .as_object()
+                            .ok_or(RosettaError::UnexpectedFormat)?
+                            .into_iter()
+                            .map(to_cont_pair)
+                            .collect::<Result<Vec<_>, _>>()?;
 
-            cont_args = cont_args_result?;
         } else {
             return Ok(all_tasks);
         }
